bc96ee2b374a4ba425ff6762172ba984eabab6fa
Julien Castro <44587467+Niwa-Yume@users.noreply.github.com>
2024-02-18 18:50:05 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "tailwind.config.js",
+    "index.html",
+    "css/style.css",
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+};
